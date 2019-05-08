62362d8a2fd3cb003228ba6145b4b3883983d83c
Markstrom, Steven L <markstro@usgs.gov>
2019-05-08 14:45:18 -0600

    works

--- ncf2cbh/cbh_updater.py
+++ ncf2cbh/cbh_updater.py
@@ -16,65 +16,8 @@ def main():
     ncf_var_names, ncf_base_date, ncf_nts, ncf_vals = ncf_reader.read(nc_fn)
     print ncf_var_names, ncf_base_date, ncf_nts, ncf_vals
 
+# TODO this is incomplete because I'm not sure if this is the right approach.
 
 if __name__ == '__main__':
     main()
 
-    # nc_fid = Dataset(nc_fn, 'r')
-    # nc_attrs = nc_fid.ncattrs()
-    # # print 'attrs', nc_attrs
-    #
-    # nc_dims = [dim for dim in nc_fid.dimensions]
-    # # print 'dims', nc_dims
-    #
-    # # Figure out the variable names with data in the ncf.
-    # nc_vars = [var for var in nc_fid.variables]
-    # remove_list = list(nc_dims)
-    # remove_list.extend(['hru_lat', 'hru_lon', 'seg_lat', 'seg_lon'])
-    # var_names = [e for e in nc_vars if e not in remove_list]
-    # # print 'var_names', var_names
-    #
-    # time = nc_fid.variables['time'][:]
-    # nts = len(time)
-    # print time, nts
-    #
-    # # TODO Need to get base_date_str from the ncf file. It's not there now
-    # base_date_str = "2019-05-05"
-    # tok = base_date_str.split('-')
-    # base_date = datetime.date(int(tok[0]), int(tok[1]), int(tok[2]))
-    # print base_date
-    #
-    # # Read the values into a dictionary.
-    # vals = {}
-    # for var in var_names:
-    #     f1 = nc_fid.variables[var][:]
-    #     vals[var] = f1
-    #
-    # nc_fid.close()
-    #
-    # # Write CBH files.
-    # for name in var_names:
-    #     v = vals[name]
-    #     nfeats = len(v[0])
-    #     fn2 = dir + name + ".cbh"
-    #     current_date = base_date
-    #     with open(fn2, 'w') as fp:
-    #         fp.write('Written by ncf2cbh.py\n')
-    #         fp.write(name + ' ' + str(nfeats) + '\n')
-    #         fp.write('########################################\n')
-    #
-    #         for ii in xrange(nts):
-    #             fp.write(str(current_date.year) + ' ' + str(current_date.month) + ' '
-    #                      + str(current_date.day) + ' 0 0 0')
-    #             for jj in xrange(nfeats):
-    #                 if name == 'prcp':
-    #                     v[ii, jj] = v[ii, jj] / 25.4
-    #                 elif name == 'tmax':
-    #                     v[ii, jj] = v[ii, jj] * 9 / 5 + 32
-    #                 elif name == 'tmin':
-    #                     v[ii, jj] = v[ii, jj] * 9 / 5 + 32
-    #                 else:
-    #                     "don't know how to convert units"
-    #                 fp.write(' ' + str(v[ii,jj]))
-    #             fp.write('\n')
-    #             current_date += datetime.timedelta(days=1)
---
+++ update_init/run_prms_to_make_init.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+def main():
+    args = "ls -lah".split()
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    print output
+
+
+if __name__ == '__main__':
+    main()
