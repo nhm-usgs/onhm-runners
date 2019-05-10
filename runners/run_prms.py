b37111e53262a727acffe7f7f8d96b6506777b66
Markstrom, Steven L <markstro@usgs.gov>
2019-05-10 11:50:50 -0600

    Added some simple test and examples of ways to run run_prms.make_init_file

--- runners/run_prms.py
+++ runners/run_prms.py
@@ -16,7 +16,7 @@ def make_init_file(st, et, prms_path, work_dir, init_flag, save_flag, control_fi
 
     if save_flag:
         save_vars_to_file = ' -set save_vars_to_file 1'
-        var_save_file = ' -set prms_save_' + st + '-' + et + '.restart'
+        var_save_file = ' -set var_save_file prms_save_' + st + '_' + et + '.restart'
     else:
         save_vars_to_file = ' -set save_vars_to_file 0'
         var_save_file = ''
@@ -25,22 +25,60 @@ def make_init_file(st, et, prms_path, work_dir, init_flag, save_flag, control_fi
 
     args = prms_path + start_args + end_args + init_vars_from_file + var_save_file + var_init_file +\
            save_vars_to_file + control_arg
+
+    print args
+
     popen = subprocess.Popen(args.split(), stdout=subprocess.PIPE, cwd=work_dir)
     popen.wait()
     output = popen.stdout.read()
     print output
 
+
 def main():
-#     latest test
     work_dir = '/work/markstro/operat/docker_test/NHM-PRMS_CONUS'
     prms_path = '/work/markstro/operat/src/prms/prms/prms'
     control_file = './NHM-PRMS.control'
-    start_time = "1980-10-1"
-    end_time = "1981-9-30"
-    init_flag = False
-    save_flag = False
-    init_file = 'prms_save_1980-10-1_1981-9-30.restart'
 
+    # Test 1 - run straight through with no init or save
+    # start_time = "1980-10-1"
+    # end_time = "1981-9-30"
+    # init_flag = False
+    # save_flag = False
+    # init_file = None
+    # make_init_file(start_time, end_time, prms_path, work_dir, init_flag, save_flag, control_file, init_file)
+
+    # Test 2 - run through and save
+    # start_time = "1980-10-1"
+    # end_time = "1981-9-30"
+    # init_flag = False
+    # save_flag = True
+    # init_file = None
+    # make_init_file(start_time, end_time, prms_path, work_dir, init_flag, save_flag, control_file, init_file)
+
+    # Test 3 - init with the file made in Test 2 and run through and save. Compare the output from this to
+    # the output of test 1 and they should be different (and they are).
+    # start_time = "1980-10-1"
+    # end_time = "1981-9-30"
+    # init_flag = True
+    # save_flag = False
+    # init_file = 'prms_save_1980-10-1_1981-9-30.restart'
+    # make_init_file(start_time, end_time, prms_path, work_dir, init_flag, save_flag, control_file, init_file)
+
+    # Test 4 - run for the first half and save. Initialize a run of the second half with the save file created
+    # by the first half and compare to the output from Test 1. Don't just run a diff on the output files because
+    # the dates don't match line by line. In other words, make sure that only lines with the same dates are compared.
+    start_time = "1980-10-1"
+    end_time = "1981-4-1"
+    init_flag = False
+    save_flag = True
+    init_file = None
+    make_init_file(start_time, end_time, prms_path, work_dir, init_flag, save_flag, control_file, init_file)
+
+    start_time = "1981-4-2"
+    end_time = "1981-9-30"
+    init_flag = True
+    save_flag = False
+    init_file = 'prms_save_1980-10-1_1981-4-1cd cd.restart'
     make_init_file(start_time, end_time, prms_path, work_dir, init_flag, save_flag, control_file, init_file)
 
 
