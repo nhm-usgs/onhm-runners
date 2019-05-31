bbebbe677705d6c20f9db913a8d78b907906d0b3
Markstrom, Steven L <markstro@usgs.gov>
2019-05-31 08:49:46 -0600

    Needed a monthly time step csv reader for dday_slope.py

--- prms_utils/csv_reader.py
+++ prms_utils/csv_reader.py
@@ -95,6 +95,60 @@ def read_output(csvfn):
             ii = ii + 1
     return nts, nfeat, base_date, end_date, vals
 
+
+# Read a PRMS "output" csv. For these files, there is a remapping in the header line that tells the order of the columns
+def read_monthly_mean(csvfn):
+    # figure out the number of features (ncol - 1)
+    # figure out the number of timesteps (nrow -1)
+    with open(csvfn, 'r') as csvfile:
+        spamreader = csv.reader(csvfile)
+
+        header = next(spamreader)
+        nfeat = len(header) - 1
+
+        ii = 0
+        for row in spamreader:
+            ii = ii + 1
+        nts = ii
+
+    vals = np.zeros(shape=(nts,nfeat))
+    indx = np.zeros(shape=nfeat, dtype=int)
+    with open(csvfn, 'r') as csvfile:
+        spamreader = csv.reader(csvfile)
+
+        # Read the header line
+        header = next(spamreader)
+        for ii in range(1,len(header)):
+            indx[ii-1] = int(header[ii])
+
+        # print(indx)
+
+        # Read the CSV file values, line-by-line, column-by-column
+        ii = 0
+        for row in spamreader:
+            jj = 0
+            kk = 0
+            for tok in row:
+                # Now skip the date/time fields and put the values into the 2D array
+                if jj > 0:
+                    try:
+                        vals[ii][indx[kk]-1] = float(tok)
+                        kk = kk + 1
+                    except:
+                        print('read_output: ', str(tok), str(ii), str(kk), str(indx[kk]-1))
+                else:
+                    # Get the base date (ie date of first time step) from the first row of values
+                    if ii == 0:
+                        base_date = tok
+                    else:
+                        end_date = tok
+                    # print(tok)
+
+                jj = jj + 1
+            ii = ii + 1
+    return nts, nfeat, base_date, end_date, vals
+
+
 def read_cbh(cbhfn):
     # figure out the number of features (ncol - 1)
     # figure out the number of timesteps (nrow -1)
