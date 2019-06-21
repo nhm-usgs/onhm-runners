6ef0bebb41e525ad32c6c2b17ce6e1977ec126d5
Richard McDonald <rmcd@usgs.gov>
2019-06-21 14:23:34 -0600

    modified ncf2cbh for docker container - temp fix

--- ncf2cbh/ncf2cbh.py
+++ ncf2cbh/ncf2cbh.py
@@ -1,8 +1,51 @@
 from netCDF4 import Dataset  # http://code.google.com/p/netcdf4-python/
+from netCDF4 import num2date
 import datetime
 from prms_utils import ncf_reader
 import sys
 
+def read(nc_fn):
+    nc_fid = Dataset(nc_fn, 'r')
+    nc_attrs = nc_fid.ncattrs()
+    # print 'attrs', nc_attrs
+
+    nc_dims = [dim for dim in nc_fid.dimensions]
+    # print 'dims', nc_dims
+
+    # Figure out the variable names with data in the ncf.
+    nc_vars = [var for var in nc_fid.variables]
+    remove_list = list(nc_dims)
+    remove_list.extend(['hru_lat', 'hru_lon', 'seg_lat', 'seg_lon'])
+    var_names = [e for e in nc_vars if e not in remove_list]
+    # print 'var_names', var_names
+
+    time = nc_fid.variables['time'][:]
+    nts = len(time)
+    # print(time, nts)
+
+    time_var = nc_fid.variables['time']
+    # print(str(time_var))
+    dtime = num2date(time_var[:],time_var.units)
+    # print("dtime = " + str(dtime[0]))
+
+    base_date_str = str(dtime[0])
+    print('base_date_str ' + base_date_str)
+    # base_date_str = "2019-05-05"
+    tok = base_date_str.split(' ')
+    ymd = tok[0]
+    tok = ymd.split('-')
+    base_date = datetime.date(int(tok[0]), int(tok[1]), int(tok[2]))
+    print(base_date)
+
+    # Read the values into a dictionary.
+    vals = {}
+    for var in var_names:
+        f1 = nc_fid.variables[var][:]
+        vals[var] = f1
+
+    nc_fid.close()
+
+    return var_names, base_date, nts, vals
 
 def main(dir, nc_fn):
     var_names, base_date, nts, vals = ncf_reader.read(nc_fn)
@@ -45,7 +88,9 @@ if __name__ == '__main__':
     if argc == 1:
         print('setting dir = ' + sys.argv[1])
         dir = sys.argv[1]
+    else:
+        dir='/var/lib/nhm/ofp/Ouput/'
 
-    nc_fn = dir + 'new.nc'
+    nc_fn = dir + 'climate_'+str(datetime.datetime.now().strftime('%Y_%m_%d'))+'.nc'
 
     main(dir, nc_fn)
